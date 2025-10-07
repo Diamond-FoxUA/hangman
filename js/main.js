7e5740b27ccd583ec6165f677311c2b44b004a19
Dmytro Farbun <dimofarb@gmail.com>
2025-10-07 20:35:20 +0300

    add functions

--- js/main.js
+++ js/main.js
@@ -1,34 +1,60 @@
-const greetFormEl = document.querySelector('.greet-form');
-const greetFormInputEl = document.querySelector('.greet-form .form-input');
-const greetFormBtn = document.querySelector('.greet-form .form-button');
+const uiElements = {
+  greetFormEl: document.querySelector('.greet-form'),
+  greetFormInputEl: document.querySelector('.greet-form .form-input'),
+  greetFormBtn: document.querySelector('.greet-form .form-button'),
+  hiddenWordEl: document.querySelector('.word'),
+  usedLetters: document.querySelector('.used-letters'),
+  triesLeft: document.querySelector('.tries-left'),
+  messageToUser: document.querySelector('.message')
+}
 
-greetFormEl.addEventListener('submit', startGame);
+uiElements.greetFormEl.addEventListener('submit', startGame);
 
 let username = 'user';
 const words = ['banana', 'apple', 'juice'];
+let usedLetters = [];
+let triesLeft = 8;
+let word;
+let guessedWord = [];
+let lettersLeft;
+
+// while (triesLeft > 0 || lettersLeft > 0) {
+//   updateGameStatus();
+//   messageUser("Guess the letter!");
+// }
 
 function startGame(event) {
   event.preventDefault();
 
-  const usernameInput = greetFormInputEl.value.trim();
+  const usernameInput = uiElements.greetFormInputEl.value.trim();
   if (!usernameInput) {
     alert("Please, enter your name.");
     return;
   }
   username = usernameInput;
   alert('Hello, ' + username);
-  greetFormEl.style.display = 'none';
-
-  const word = words[Math.floor(Math.random() * words.length)];
-  const guessedWord = [];
-  for (let i = 0; i < word.length; i ++) {
+  hideGreet();
+  
+  word = words[Math.floor(Math.random() * words.length)];
+  for (let i = 0; i < word.length; i++) {
     guessedWord.push('_');
   }
+  alert(`Welcome, ${username}!`);
+
   console.log(guessedWord);
-
-  // show ui, etc
-  
-
   console.log(word);
-  
+}
+
+function updateGameStatus() {
+  uiElements.hiddenWordEl.textContent = guessedWord.join(' ');
+  uiElements.usedLetters.textContent = "Used letters: " + usedLetters.join(' ');
+  uiElements.triesLeft.textContent = "Tries left: " + triesLeft;
+}
+
+function messageUser(message) {
+  uiElements.messageToUser.textContent = message;
+}
+
+function hideGreet() {
+  uiElements.greetFormEl.style.display = 'none';
 }
